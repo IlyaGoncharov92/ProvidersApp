64dac6222e891757d45707026e2de87769a42d7d
Ilya <ilyagoncharov92@gmail.com>
2024-06-24 14:11:48 +0300

    Init AddMemoryCache

--- ProvidersApp/Api/Program.cs
+++ ProvidersApp/Api/Program.cs
@@ -4,13 +4,15 @@ using Api.Services;
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
+services.AddAutoMapper(typeof(AutoMapperConfig));
+
 services.AddSingleton<ISearchService, SearchService>();
 services.AddSingleton<IProvidersService, ProvidersService>();
 services.AddSingleton<IProvidersHttpService, ProvidersHttpHttpService>();
 
 services.AddControllers();
 
-services.AddAutoMapper(typeof(AutoMapperConfig));
+services.AddMemoryCache();
 
 var app = builder.Build();
 
